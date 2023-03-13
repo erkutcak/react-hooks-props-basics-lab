105f66e17b3d48cc57ea7e37139d2c81c1d765b5
Erkut Cakmak <cakmak.erkut1@gmail.com>
2023-03-13 13:50:09 -0400

    lab complete

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,18 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About({bio, links}) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bio ? <p>{bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links/>
     </div>
   );
 }
 
 export default About;
+
+// they're doing maintenance on the HVAC it's a 
+// little loud lol I cant turn my mic on
